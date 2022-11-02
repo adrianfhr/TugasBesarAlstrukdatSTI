97c57d4df5c1c7fcbc87080dd2ddacd852e037c7
fadhil <undead.gamer313@gmail.com>
2022-11-02 21:45:10 +0700

    game

--- exit.h
+++ exit.h
@@ -1,7 +1,7 @@
 #ifndef EXIT
 #define EXIT
+#include <stdio.h>
 void exit(int status);
 /*exit(0) untuk langsung exit dari main p.s print"Anda keluar dari game BNMO.
-Bye bye ..." sebelum di exit
-*/
+Bye bye ..." sebelum di exit*/
 #endif
