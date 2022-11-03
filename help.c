62967c7c6623ff151c1ac50cf51b5488fc8df40a
Eleanora Felicia <18221032@std.stei.itb.ac.id>
2022-11-03 19:14:47 +0700

    help

--- help.c
+++ help.c
@@ -2,12 +2,12 @@ void HELP(){
     printf("1. START        <Menjalankan BNMO> \n");
     printf("2. LOAD         <Membaca savefile> \n");
     printf("3. SAVE         <Menyimpan state game pemain saat ini ke dalam file> \n");
-    printf("4. CREATEGAME   <Menambah game baru pada daftar game> \n");
+    printf("4. CREATE       <Menambah game baru pada daftar game> \n");
     printf("5. LISTGAME     <Menampilkan daftar game> \n");
-    printf("6. DELETEGAME   <Menghapus suatu game dari daftar game> \n");
-    printf("7. QUEUEGAME    <Mendaftarkan permainan kedalam daftar antrian game> \n");
-    printf("8. PLAYGAME     <Memainkan sebuah permainan> \n");
-    printf("9. SKIPGAME     <Melewatkan permainan> \n");
+    printf("6. DELETE       <Menghapus suatu game dari daftar game> \n");
+    printf("7. QUEUE        <Mendaftarkan permainan kedalam daftar antrian game> \n");
+    printf("8. PLAY         <Memainkan sebuah permainan> \n");
+    printf("9. SKIP         <Melewatkan permainan> \n");
     printf("10. QUIT        <Keluar dari program> \n");
     printf("11. HELP        <Bantuan command-command> \n");
 }
