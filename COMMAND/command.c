bd55ac94a79cb56ef5bda1f72d2496927dabd562
Eleanora Felicia <18221032@std.stei.itb.ac.id>
2022-11-03 19:10:18 +0700

    edit help

--- COMMAND/command.c
+++ COMMAND/command.c
@@ -42,18 +42,17 @@ void SKIPGAME();
 void QUIT();
 
 void HELP(){
-    printf("Command yang tersedia: /n");
-    printf("1. START/n");
-    printf("2. LOAD/n");
-    printf("3. SAVE/n");
-    printf("4. CREATEGAME/n");
-    printf("5. LISTGAME/n");
-    printf("6. DELETEGAME/n");
-    printf("7. QUEUEGAME/n");
-    printf("8. PLAYGAME/n");
-    printf("9. SKIPGAME/n");
-    printf("10. QUIT/n");
-    printf("11. HELP/n");
+    printf("1. START        <Menjalankan BNMO> \n");
+    printf("2. LOAD         <Membaca savefile> \n");
+    printf("3. SAVE         <Menyimpan state game pemain saat ini ke dalam file> \n");
+    printf("4. CREATEGAME   <Menambah game baru pada daftar game> \n");
+    printf("5. LISTGAME     <Menampilkan daftar game> \n");
+    printf("6. DELETEGAME   <Menghapus suatu game dari daftar game> \n");
+    printf("7. QUEUEGAME    <Mendaftarkan permainan kedalam daftar antrian game> \n");
+    printf("8. PLAYGAME     <Memainkan sebuah permainan> \n");
+    printf("9. SKIPGAME     <Melewatkan permainan> \n");
+    printf("10. QUIT        <Keluar dari program> \n");
+    printf("11. HELP        <Bantuan command-command> \n");
 }
 
 void COMMANDLAIN()
