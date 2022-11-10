620e47071e4a8a340af7967b5fd3ec4608165eb8
fadhil <undead.gamer313@gmail.com>
2022-11-10 17:29:27 +0700

    game 1 dan 3

---
+++ src/GAME/game/rng.h
@@ -0,0 +1,12 @@
+#ifndef RNG
+#define RNG
+#include <stdio.h>
+#include <time.h>
+#include "boolean.h"
+
+int tebakan();
+//mendapatkan angka yang ingin di tebak
+
+int playgame(int tebak);
+
+#endif
