cf8f565b55762771625930dd97a10d90844a26c2
fadhil <undead.gamer313@gmail.com>
2022-11-11 20:13:05 +0700

    driver

---
+++ src/ADT/driver_arraydin.c
@@ -0,0 +1,19 @@
+#include"arraydin.c"
+#include<stdio.h>
+int main(){
+    ArrayDin array;
+    array=MakeArrayDin();
+    if (IsEmpty(array)){
+        printf("panjang = %d\n",Length(array));
+        printf("kapasitas = %d\n",GetCapacity(array));
+        InsertAt(&array,"t",5);
+        printf("panjang = %d\n",Length(array));
+        if (IsEmpty(array)){
+            DeleteAt(&array,5);
+            DeallocateArrayDin(&array);
+        }
+    }else{
+        printf("array tidak kosong\n");
+    }
+    return 0;
+}
