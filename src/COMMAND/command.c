1f292fd703466083d72129595867905f7c72502d
Eleanora Felicia <18221032@std.stei.itb.ac.id>
2022-11-11 18:08:32 +0700

    update command

--- src/COMMAND/command.c
+++ src/COMMAND/command.c
@@ -328,12 +328,12 @@ void HELP(){
     printf("1. START        <Menjalankan BNMO> \n");
     printf("2. LOAD         <Membaca savefile> \n");
     printf("3. SAVE         <Menyimpan state game pemain saat ini ke dalam file> \n");
-    printf("4. CREATE       <Menambah game baru pada daftar game> \n");
-    printf("5. LISTGAME     <Menampilkan daftar game> \n");
-    printf("6. DELETE       <Menghapus suatu game dari daftar game> \n");
+    printf("4. CREATE GAME       <Menambah game baru pada daftar game> \n");
+    printf("5. LIST GAME     <Menampilkan daftar game> \n");
+    printf("6. DELETE GAME       <Menghapus suatu game dari daftar game> \n");
     printf("7. QUEUE        <Mendaftarkan permainan kedalam daftar antrian game> \n");
-    printf("8. PLAY         <Memainkan sebuah permainan> \n");
-    printf("9. SKIP         <Melewatkan permainan> \n");
+    printf("8. PLAY GAME        <Memainkan sebuah permainan> \n");
+    printf("9. SKIPGAME         <Melewatkan permainan> \n");
     printf("10. QUIT        <Keluar dari program> \n");
     printf("11. HELP        <Bantuan command-command> \n");
 }
