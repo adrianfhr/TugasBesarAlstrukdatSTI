22bf4c6ebbe2d50cb1e2354c9389774a0ced21be
adrianfhr <adrianfahriaffandi@gmail.com>
2022-11-21 21:14:44 +0700

    UPDATE BNMO

--- bin/compile.sh
+++ bin/compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gcc  -o BNMO ../src/main.c ../src/COMMAND/command.c ../src/GAME/game.c ../src/GAME/diner_dash.c ../src/ADT/arraydin.c ../src/ADT/mesinkar.c ../src/ADT/mesinkata.c ../src/ADT/queue.c 
+gcc  -o BNMO ../src/main.c ../src/COMMAND/command.c ../src/GAME/game.c ../src/GAME/diner_dash.c ../src/ADT/ARRAYDIN/arraydin.c ../src/ADT/MESINKATA/mesinkar.c ../src/ADT/MESINKATA/mesinkata.c ../src/ADT/QUEUE/queue.c
