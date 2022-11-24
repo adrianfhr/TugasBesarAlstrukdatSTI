ce93db82dd4613d183ca95e10bd443c3fed72355
adrianfhr <adrianfahriaffandi@gmail.com>
2022-11-24 13:31:41 +0700

    repair stack

--- src/ADT/STACK/stack.c
+++ src/ADT/STACK/stack.c
@@ -5,24 +5,24 @@
 /* *** Konstruktor/Kreator *** */
 void CreateEmpty(Stack *S)
 /* I.S. sembarang; */
-/* F.S. Membuat sebuah stack S yang kosong berkapasitas MaxEl */
-/* jadi indeksnya antara 0.. MaxEl */
-/* Ciri stack kosong : TOP bernilai Nil */
+/* F.S. Membuat sebuah stack S yang kosong berkapasitas CAPACITY */
+/* jadi indeksnya antara 0.. CAPACITY */
+/* Ciri stack kosong : TOP bernilai NilStack */
 {
-    Top(*S) = Nil;
+    Top(*S) = NilStack;
 }
 
 /* ************ Predikat Untuk test keadaan KOLEKSI ************ */
 boolean IsEmpty(Stack S)
 /* Mengirim true jika Stack kosong: lihat definisi di atas */
 {
-    return Top(S) == Nil;
+    return Top(S) == NilStack;
 }
 
 boolean IsFull(Stack S)
 /* Mengirim true jika tabel penampung nilai elemen stack penuh */
 {
-    return Top(S) == MaxEl - 1;
+    return Top(S) == CAPACITY - 1;
 }
 
 /* ************ Menambahkan sebuah elemen ke Stack ************ */
@@ -50,12 +50,12 @@ void Push(Stack *S, ElType X)
 void Pop(Stack *S, ElType *X)
 /* Menghapus X dari Stack S. */
 /* I.S. S  tidak mungkin kosong */
-/* F.S. X adalah nilai elemen TOP yang lama, TOP berkurang 1 */
+/* F.S. X adalah NilStackai elemen TOP yang lama, TOP berkurang 1 */
 {
     *X = InfoTop(*S);
     if (Top(*S) == 0)
     {
-        Top(*S) = Nil;
+        Top(*S) = NilStack;
     }
     else
     {
--- src/ADT/STACK/stack.h
+++ src/ADT/STACK/stack.h
@@ -6,30 +6,29 @@
 #define stackt_H
 
 #include "boolean.h"
-#include "arraydin.h"
+#include "../ARRAYDIN/arraydin.h"
+#include "../QUEUE/queue.h"
 
-#define Nil -1
-#define MaxEl 5
-/* Nil adalah stack dengan elemen kosong . */
+#define NilStack -1
+/* NilStack adalah stack dengan elemen kosong . */
 
-typedef int ElType;
 typedef int address;   /* indeks tabel */
 
 /* Contoh deklarasi variabel bertype stack dengan ciri TOP : */
 /* Versi I : dengan menyimpan tabel dan alamat top secara eksplisit*/
 typedef struct { 
-  ElType T[MaxEl]; /* tabel penyimpan elemen */
+  ElType T[CAPACITY]; /* tabel penyimpan elemen */
   address TOP;  /* alamat TOP: elemen puncak */
 } Stack;
-/* Definisi stack S kosong : S.TOP = Nil */
-/* Elemen yang dipakai menyimpan nilai Stack T[0]..T[MaxEl-1] */
+/* Definisi stack S kosong : S.TOP = NilStack */
+/* Elemen yang dipakai menyimpan NilStackai Stack T[0]..T[MaxEl-1] */
 /* Jika S adalah Stack maka akses elemen : */
    /* S.T[(S.TOP)] untuk mengakses elemen TOP */
    /* S.TOP adalah alamat elemen TOP */
 
 /* Definisi akses dengan Selektor : Set dan Get */
 #define Top(S) (S).TOP
-#define ElTop(S) (S).T[(S).TOP]
+#define InfoTop(S) (S).T[(S).TOP]
 
 /* ************ Prototype ************ */
 /* *** Konstruktor/Kreator *** */
@@ -37,13 +36,13 @@ void CreateEmpty(Stack *S);
 /* I.S. sembarang; */
 /* F.S. Membuat sebuah stack S yang kosong berkapasitas MaxEl */
 /* jadi indeksnya antara 0.. MaxEl */
-/* Ciri stack kosong : TOP bernilai Nil */
+/* Ciri stack kosong : TOP berNilStackai NilStack */
 
 /* ************ Predikat Untuk test keadaan KOLEKSI ************ */
 boolean IsEmpty(Stack S);
 /* Mengirim true jika Stack kosong: lihat definisi di atas */
 boolean IsFull(Stack S);
-/* Mengirim true jika tabel penampung nilai elemen stack penuh */
+/* Mengirim true jika tabel penampung NilStackai elemen stack penuh */
 
 /* ************ Menambahkan sebuah elemen ke Stack ************ */
 void Push(Stack * S, ElType X);
@@ -55,6 +54,6 @@ void Push(Stack * S, ElType X);
 void Pop(Stack * S, ElType* X);
 /* Menghapus X dari Stack S. */
 /* I.S. S  tidak mungkin kosong */
-/* F.S. X adalah nilai elemen TOP yang lama, TOP berkurang 1 */
+/* F.S. X adalah NilStackai elemen TOP yang lama, TOP berkurang 1 */
 
 #endif
