38c21f0323e6ea3e7ed903c8edae88096a79eae4
fadhil <undead.gamer313@gmail.com>
2022-12-02 20:10:56 +0700

    99.9?

--- src/COMMAND/command.c
+++ src/COMMAND/command.c
@@ -28,7 +28,7 @@ void commandmenu(){
         printf("3. DELETE GAME\n");
         printf("4. QUEUE GAME\n");
         printf("5. PLAY GAME\n");
-        printf("6. SKIPGAME\n");
+        printf("6. SKIPGAME <n>\n");
         printf("7. SCOREBOARD\n");
         printf("8. RESET SCOREBOARD\n");
         printf("9. HISTORY <n>\n");
@@ -438,16 +438,16 @@ void HELP(){
     printf("Daftar perintah yang tersedia:\n");
     printf("1. START                <Menjalankan BNMO> \n");
     printf("2. LOAD                 <Membaca savefile> \n");
-    printf("3. SAVE                 <Menyimpan state game pemain saat ini ke dalam file> \n");
+    printf("3. SAVE <txt>           <Menyimpan state game pemain saat ini ke dalam file> \n");
     printf("4. CREATE GAME          <Menambah game baru pada daftar game> \n");
     printf("5. LIST GAME            <Menampilkan daftar game> \n");
     printf("6. DELETE GAME          <Menghapus suatu game dari daftar game> \n");
     printf("7. QUEUE GAME           <Mendaftarkan permainan kedalam daftar antrian game> \n");
     printf("8. PLAY GAME            <Memainkan sebuah permainan> \n");
-    printf("9. SKIPGAME             <Melewatkan permainan> \n");
+    printf("9. SKIPGAME <n>         <Melewatkan permainan> \n");
     printf("10. SCOREBOARD          <Menampilkan nama dan skor untuk semua game> \n");
     printf("11. RESET SCOREBOARD    <Melakukan reset terhadap scoreboard> \n");
-    printf("12. HISTORY             <Menampilkan history permainan yang telah dimainkan> \n");
+    printf("12. HISTORY <n>         <Menampilkan history permainan yang telah dimainkan> \n");
     printf("13. RESET HISTORY       <Menghapus semua history permainan yang dimainkan> \n");
     printf("14. QUIT                <Keluar dari program> \n");
     printf("15. HELP                <Bantuan command-command> \n");
